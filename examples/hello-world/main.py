e5682381e7ccd206362b000e664f0e3884482773
arsin-dev <mail@arsin.io>
2025-06-19 12:07:38 -1000

    wip test local example

--- examples/hello-world/main.py
+++ examples/hello-world/main.py
@@ -9,7 +9,7 @@ def add(a: int, b: int) -> int:
 
 if __name__ == "__main__":
     # This runs the server, defaulting to STDIO transport
-    mcp.run()
+    # mcp.run()
 
     # To use a different transport, e.g., HTTP:
-    # mcp.run(transport="streamable-http", host="127.0.0.1", port=9000)
+    mcp.run(transport="streamable-http", host="127.0.0.1", port=9000)
